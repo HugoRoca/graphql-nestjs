948804826d2bed4718884b4c5569bd46fa8c212e
HugoRoca <hugo.rock20@hotmail.com>
2024-02-02 23:26:35 -0500

    install apollo studio

--- 02-graphql/src/hello-world/hello-world.resolver.ts
+++ 02-graphql/src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,23 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver, Int, Args } from "@nestjs/graphql";
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, { name: "hello", description: "A simple hello world query"})
   helloWorld(): string {
     return 'Hello, world!';
+  }
+
+  @Query(() => Float, { name: "random", description: "A simple random number query"})
+  getRandomNumber(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, { name: "randomFromZeroTo", description: "From zero to argument to (default 6)" })
+  getRandomFromZeroTo(
+    @Args("to", {
+      type: () => Int, // For valid argument type
+      nullable: true, // For optional argument
+    }) to: number = 6): number {
+    return Math.floor(Math.random() * to);
   }
 }
